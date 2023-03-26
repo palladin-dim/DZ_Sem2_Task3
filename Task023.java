c66a0467a8fb0d37ef762a7131ef92d105812f26
Dmityi <palladin-dim@mail.ru>
2023-03-26 21:30:50 +1100

    Task_3

---
+++ Task023.java
@@ -0,0 +1,18 @@
+/* 
+Напишите метод, который принимает на вход строку (String) и определяет является ли строка палиндромом (возвращает boolean значение). Без встр. функций
+*/
+public class Task023 {
+    public static void main(String[] args) {
+        String input1 = "Палиндром";
+        String input2 = "ротор";
+        System.out.println(input1 + " " + input2);
+        System.out.println(isPalindrome(input1) + " " + isPalindrome(input2));
+    }
+
+    public static boolean isPalindrome(String input) {
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) return false;
+        }
+        return true;
+    }
+}
